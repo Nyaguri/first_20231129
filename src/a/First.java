428f5a0f52b6a7c204294c2a0fcd98492a746dd3
nyaguri <tsuki4026@gmail.com>
2023-11-29 15:39:25 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,15 @@
+// 현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+// 현재 클래스의 이름
+public class First { // First class 블록의 시작
+
+	// main 매서드(프로그램의 시작 지점)
+	public static void main(String[] args) { // main 매서드 블록의 시작
+		// TODO Auto-generated method stub
+			// 실행할 내용이 작성되는 공간
+		System.out.println("자바 개새끼");
+		
+	}// main 매서드 블록의 끝
+
+} // First class 블록의 끝
